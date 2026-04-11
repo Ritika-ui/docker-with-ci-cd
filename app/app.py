b08b9d35c9b59076eaf1245ebb81d38bf8511b4b
Ritika Khadka <ritikakhadka155@gmail.com>
2026-04-11 02:49:12 +0000

    yeahhh

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "First Project LIVEEEEE!!!!! 🚀"
+    return "Hello Devopssss LIVEEEEE!!!!! 🚀"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
