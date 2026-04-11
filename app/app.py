d04efd8f0b7908db76b261ee2aea6b98fe6edbca
Ritika Khadka <ritikakhadka155@gmail.com>
2026-04-11 02:41:34 +0000

    rerun

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Clean DevOps Project 🚀"
+    return "First Project LIVEEEEE!!!!! 🚀"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
